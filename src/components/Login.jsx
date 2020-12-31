a44be2c7a467aef766f9dc7a5e4f848bffd56d22
unknown <heidiybang@gmail.com>
2020-12-30 18:12:55 -0800

    working login and signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,96 +2,107 @@
  * @name Login
  * @desc Displays login screen
  */
+import React, { useState } from 'react';
+import { Switch, Route, Link, useHistory } from 'react-router-dom';
+import { render } from 'react-dom';
+import axios from 'axios';
 
-import React, { Component } from 'react';
-
-export default class Login extends Component {
-  constructor() {
-    super();
-
-    // Initial state (empty strings) for the form's input fields.
-    this.state = {
-      username: '',
-      password: '',
-      match: false
-    };
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+function Login(props) {
+  const history = useHistory();
+  const [state, setState] = useState({
+    username: '',
+    password: '',
+    authorized: false
+  });
 
   // OnChange event handler that will update state whenever a key is pressed in an input field.
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  }
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-  //OnSubmit event handler on the form that will do a POST request to MongoDB with the current state as the request body.
-  async onSubmit(e) {
-    e.preventDefault();
-    const newLogin = { username, password } = this.state;
-    //const history = this.props;
 
-    try {
-
-      //query database to find if user exists
-      //if so, does username match password?
-      //if so, redirect
-      //if not, reload this page
-      await axios.get('/api/newLogin', body, {
+  const onSubmit = (e) => {
+  
+   e.preventDefault(e);
+    if (state.username.length && state.password.length) {
+      const userData = {
+        "username": state.username,
+        "password": state.password
+      };
+      
+      axios.post('/api/verify', userData, { 
         headers: { 'content-type': 'application/json' },
-      });
-      await this.setState({
-        username: '',
-        password: ''
-      });
+      }).then((res) => {
 
-    } catch (err) {
-      console.log(err);
+        console.log('hi')
+        if (res.status === 200) {
+          setState((prevState) => ({
+            ...prevState,
+            Authentication: 'true',
+          }));
+
+          //need sesssionStorage stuff here...
+          //sessionStorage.setItem('loggedInUser', state.username.toLowerCase());
+          redirectToTicketForm();
+        }
+      }).catch((err) => console.log(err, 'line 56'));
+    } else {
+      console.log('err');
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="row justify-content-center mt-5">
-        <div className="col-6">
-          <form onSubmit={this.onSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
-            <label>
-              USERNAME:
+  const redirectToTicketForm = () => {
+    history.push('/ticketform')
+  };
+
+  return (
+    <div className="row justify-content-center mt-5">
+      <div className="col-6">
+        <form onSubmit={onSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
+          <label>
+            USERNAME:
               <input
-                className="form-control"
-                onChange={this.onChange}
-                type="text"
-                name="username"
-                value={this.state.username}
-                required
-              />
-            </label>
-            <label>
-              PASSWORD:
+              className="form-control"
+              onChange={onChange}
+              type="text"
+              name="username"
+              value={state.username}
+              required
+            />
+          </label>
+          <label>
+            PASSWORD:
               <input
-                className="form-control"
-                style={{ width: '100%' }}
-                onChange={this.onChange}
-                type="password"
-                name="password"
-                value={this.state.password}
-                required
-              />
-            </label>
-            <br/>
-            <button className="btn btn-success" type="submit">
-              LOGIN
+              className="form-control"
+              style={{ width: '100%' }}
+              onChange={onChange}
+              type="password"
+              name="password"
+              value={state.password}
+              required
+            />
+          </label>
+          <br />
+          
+          <button className="btn" type="submit">
+            LOGIN
             </button>
-              <br/>
-              <p style={{textAlign: 'center'}}>OR</p>
-
-            <button className="btn btn-success" type="submit">
-            CREATE ACCOUNT
-          </button>
-          </form>
-        </div>
+          <br />
+          <p style={{ textAlign: 'center' }}>OR</p>
+          <Link className="btn" to="/signup">   
+            <button className="btn create" type="button">
+              CREATE ACCOUNT
+            </button>
+          </Link>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+
+export default Login;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,111 @@
 /**
  * @name Signup
- * @desc Displays signup screen
+ * @desc Displays signup screen/**
  */
 
+import React, { useState } from 'react';
+import { Switch, Route, Link, useHistory } from 'react-router-dom';
+import { render } from 'react-dom';
+import axios from 'axios';
+
+function Signup(props) {
+ const history = useHistory();
+ const [state, setState] = useState({
+      username: '',
+      password: '',
+      authorized: false
+ });
+
+  // OnChange event handler that will update state whenever a key is pressed in an input field.
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (state.username.length && state.password.length) {
+      const userData = {
+        "username": state.username,
+        "password": state.password
+      };
+ 
+      axios.post('/api/create', userData, { //do we need the /api???
+        headers: { 'content-type': 'application/json' },
+      }).then((res) => {
+        if (res.status === 200) {
+          setState((prevState) => ({
+            ...prevState,
+            Authentication: 'true',
+          }));
+ 
+//need sesssionStorage stuff here...
+          //sessionStorage.setItem('loggedInUser', state.username.toLowerCase());
+          redirectToTicketForm();
+        }
+      });
+    } else {
+        console.log('err');
+      }
+  };
+
+  const redirectToTicketForm = () => {
+    history.push('/ticketform')
+  };
+  
+    return (
+      <div className="row justify-content-center mt-5">
+        <div className="col-6">
+          <form onSubmit={onSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
+            <label>
+              USERNAME:
+              <input
+                className="form-control"
+                onChange={onChange}
+                type="text"
+                name="username"
+                value={state.username}
+                required
+              />
+            </label>
+            <label>
+              PASSWORD:
+              <input
+                className="form-control"
+                style={{ width: '100%' }}
+                onChange={onChange}
+                type="password"
+                name="password"
+                value={state.password}
+                required
+              />
+            </label>
+            
+            <label>
+              FELLOW?:
+              <input
+                className="form-control"
+                style={{ width: '100%' }}
+                onChange={onChange}
+                type="checkbox"
+                name="authorized"
+                value={state.authorized}
+                required
+              />
+            </label>
+
+            <button className="btn btn-success" type="button">
+            CREATE ACCOUNT
+          </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+
+export default Signup;
